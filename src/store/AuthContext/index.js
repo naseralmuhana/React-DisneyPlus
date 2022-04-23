6526665a3038e51cb211acb44f0bf00d7045d350
naseralmuhana0 <naseralmuhana0@gmail.com>
2022-04-23 04:19:15 +0300

    Updated!

--- src/store/AuthContext/index.js
+++ src/store/AuthContext/index.js
@@ -1,112 +1,37 @@
 import { signInWithPopup, signOut } from "firebase/auth"
-import React, {
-  useCallback,
-  useContext,
-  useEffect,
-  useReducer
-} from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { auth, provider } from "../../firebase"
-import { calculateRemainingTime, retrieveStoredToken } from "./helper"
+import useLocalStorage from "../../hooks/use-localStorage"
 
 const AuthContext = React.createContext({
   currentUser: {},
-  token: "",
   isLoggedIn: false,
   login: async () => {},
   logout: async () => {},
 })
 
-let logoutTimer
-
-const authReducer = (state, action) => {
-  switch (action.type) {
-    case "LOGIN":
-      const { currentUser, token, expiresInTS } = action.data
-      const expiresInDate = new Date(expiresInTS)
-
-      // save to localStorage
-      localStorage.setItem("token", token)
-      localStorage.setItem("expiresInDate", expiresInDate)
-
-      return {
-        currentUser,
-        token,
-        isLoggedIn: !!token,
-      }
-
-    case "LOGOUT":
-      // remove from localStorage
-      localStorage.removeItem("token")
-      localStorage.removeItem("expiresInDate")
-
-      return {
-        currentUser: {},
-        token: null,
-        isLoggedIn: false,
-      }
-
-    default:
-      return state
-  }
-}
-
 export const AuthProvider = ({ children }) => {
-  const tokenData = retrieveStoredToken()
-
-  let initialToken = null
-  if (tokenData) {
-    initialToken = tokenData.token
-  }
-
-  const [authState, dispatch] = useReducer(authReducer, {
-    currentUser: {},
-    token: initialToken,
-    isLoggedIn: !!initialToken,
-  })
+  console.log("AuthProvider")
+  const [currentUser, setCurrentUser] = useState(null)
+  const [isLoggedIn, setIsLoggedIn] = useLocalStorage("isLoggedIn", false)
 
   // Login
-  const login = async () => {
-    const responseData = await signInWithPopup(auth, provider)
-    // Data
-    const currentUser = responseData.user
-    const token = currentUser.stsTokenManager.accessToken
-    const expiresInTS = currentUser.stsTokenManager.expirationTime // TimeStamp
-    const remainingTime = calculateRemainingTime(expiresInTS) // milliseconds
-
-    const data = {
-      currentUser,
-      token,
-      expiresInTS,
-    }
-    dispatch({ type: "LOGIN", data })
-
-    logoutTimer = setTimeout(() => {
-      logout()
-    }, remainingTime)
-  }
+  const login = async () => await signInWithPopup(auth, provider)
 
   // Logout
-  const logout = useCallback(async () => {
-    await signOut(auth)
-    dispatch({ type: "LOGOUT" })
-
-    if (logoutTimer) {
-      clearTimeout(logoutTimer)
-    }
-  }, [])
+  const logout = async () => await signOut(auth)
 
   useEffect(() => {
-    if (tokenData) {
-      logoutTimer = setTimeout(() => {
-        logout()
-      }, tokenData.remainingTime)
-    }
-  }, [tokenData, logout])
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user)
+      setIsLoggedIn(!!user)
+    })
+    return unsubscribe
+  }, [])
 
   const contextValue = {
-    currentUser: authState.currentUser,
-    token: authState.token,
-    isLoggedIn: authState.isLoggedIn,
+    currentUser,
+    isLoggedIn,
     login,
     logout,
   }
