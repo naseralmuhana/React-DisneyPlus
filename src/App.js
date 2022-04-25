b3f2f17d9b243f229ffae94305a60de8d5507f4f
naseralmuhana0 <naseralmuhana0@gmail.com>
2022-04-26 02:12:50 +0300

    Updated!

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from "react"
+import React, { Suspense } from "react"
 import { Navigate, Route, Routes } from "react-router-dom"
 import ProtectedRoutes from "./components/ProtectedRoutes"
-import { Detail, Header, Home, Landing } from "./containers"
+import Loader from "./components/UI/Loader"
+// import { Detail, Header, Home, Landing } from "./containers"
 import { useAuth } from "./store/auth-context"
 
-// const Header = React.lazy(() => import("./containers/Header"))
-// const Landing = React.lazy(() => import("./containers/Landing"))
-// const Home = React.lazy(() => import("./containers/Home"))
+const Header = React.lazy(() => import("./containers/Header"))
+const Landing = React.lazy(() => import("./containers/Landing"))
+const Home = React.lazy(() => import("./containers/Home"))
+const Detail = React.lazy(() => import("./containers/Detail"))
 
 const App = () => {
   const { isLoggedIn } = useAuth()
   return (
-    <>
+    <Suspense fallback={<Loader />}>
       <Header />
       <Routes>
         {!isLoggedIn && <Route path="/" element={<Landing />} />}
@@ -21,7 +23,7 @@ const App = () => {
         </Route>
         <Route path="*" element={<Navigate to={"home"} />} />
       </Routes>
-    </>
+    </Suspense>
   )
 }
 
